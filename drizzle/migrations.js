b6eb7f3b25eaed6cd5b10db0cebe6e1ce458b872
sofronijev <sofronijev@gmail.com>
2024-10-27 15:37:44 +0100

    seed categories

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -4,13 +4,15 @@ import journal from './meta/_journal.json';
 import m0000 from './0000_flawless_reavers.sql';
 import m0001 from './0001_seed-user.sql';
 import m0002 from './0002_seed-wallet.sql';
+import m0003 from './0003_seed-categories.sql';
 
   export default {
     journal,
     migrations: {
       m0000,
 m0001,
-m0002
+m0002,
+m0003
     }
   }
   
