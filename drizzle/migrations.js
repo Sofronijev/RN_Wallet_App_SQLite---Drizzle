b16c3190602e704d1d0147dd92af9d7c49b71f84
sofronijev <sofronijev@gmail.com>
2024-10-27 15:28:04 +0100

    wallet seed

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -3,12 +3,14 @@
 import journal from './meta/_journal.json';
 import m0000 from './0000_flawless_reavers.sql';
 import m0001 from './0001_seed-user.sql';
+import m0002 from './0002_seed-wallet.sql';
 
   export default {
     journal,
     migrations: {
       m0000,
-m0001
+m0001,
+m0002
     }
   }
   
