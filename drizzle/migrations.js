6063881f4ab30fb675d27d3a25bb308965a726fb
sofronijev <sofronijev@gmail.com>
2024-10-27 15:39:36 +0100

    seed types

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -5,6 +5,7 @@ import m0000 from './0000_flawless_reavers.sql';
 import m0001 from './0001_seed-user.sql';
 import m0002 from './0002_seed-wallet.sql';
 import m0003 from './0003_seed-categories.sql';
+import m0004 from './0004_seed-types.sql';
 
   export default {
     journal,
@@ -12,7 +13,8 @@ import m0003 from './0003_seed-categories.sql';
       m0000,
 m0001,
 m0002,
-m0003
+m0003,
+m0004
     }
   }
   
