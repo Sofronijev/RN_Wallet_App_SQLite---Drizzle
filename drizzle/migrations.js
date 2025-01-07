8b1a77787f90226489cdb82c27874315e4141150
sofronijev <sofronijev@gmail.com>
2025-01-07 10:10:03 +0100

    migration for transfer table

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -10,6 +10,7 @@ import m0005 from './0005_left_pet_avengers.sql';
 import m0006 from './0006_slow_falcon.sql';
 import m0007 from './0007_ordinary_slipstream.sql';
 import m0008 from './0008_early_purifiers.sql';
+import m0009 from './0009_puzzling_stardust.sql';
 
   export default {
     journal,
@@ -22,7 +23,8 @@ m0004,
 m0005,
 m0006,
 m0007,
-m0008
+m0008,
+m0009
     }
   }
   
