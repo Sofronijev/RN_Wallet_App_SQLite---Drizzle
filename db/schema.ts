7759f0c8f2fd5d4185518ff256776b7bf5f5d7d1
sofronijev <sofronijev@gmail.com>
2025-01-08 22:23:39 +0100

    fix default value for currency in table

--- db/schema.ts
+++ db/schema.ts
@@ -42,8 +42,8 @@ export const wallet = sqliteTable("Wallet", {
     .notNull(),
   startingBalance: real("startingBalance").default(0).notNull(),
   walletName: text("walletName", { length: 255 }).default("My custom wallet").notNull(),
-  currencyCode: text("currencyCode", { length: 10 }),
-  currencySymbol: text("currencySymbol", { length: 10 }),
+  currencyCode: text("currencyCode", { length: 10 }).default(""),
+  currencySymbol: text("currencySymbol", { length: 10 }).default(""),
   type: text("type", { length: 255, enum: ["custom", "system"] })
     .default("custom")
     .notNull(),
