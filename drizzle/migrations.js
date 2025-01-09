874d95fd8cc7c5321add505bdfae0c8e6e97d37a
sofronijev <sofronijev@gmail.com>
2025-01-09 19:27:58 +0100

    seed user and wallets

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -2,11 +2,15 @@
 
 import journal from './meta/_journal.json';
 import m0000 from './0000_aberrant_rawhide_kid.sql';
+import m0001 from './0001_seedUser.sql';
+import m0002 from './0002_seedWallets.sql';
 
   export default {
     journal,
     migrations: {
-      m0000
+      m0000,
+m0001,
+m0002
     }
   }
   
