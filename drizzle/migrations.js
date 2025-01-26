3b8529cc90c25bbbeaed3748d7c163e6603db4ac
sofronijev <sofronijev@gmail.com>
2025-01-26 19:28:49 +0100

    seed wallets

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -3,11 +3,13 @@
 import journal from "./meta/_journal.json";
 import m0000 from "./0000_parched_cyclops.sql";
 import m0001 from "./0001_seedUser.sql";
+import m0002 from "./0002_seedWallet.sql";
 
 export default {
   journal,
   migrations: {
     m0000,
     m0001,
+    m0002,
   },
 };
