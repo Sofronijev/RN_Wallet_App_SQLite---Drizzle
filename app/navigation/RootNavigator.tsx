a6bdd25015f573a62c1815c9e2d148ff9e0456e7
sofronijev <sofronijev@gmail.com>
2025-12-28 18:48:29 +0100

    Hide action sheet on pin show

--- app/navigation/RootNavigator.tsx
+++ app/navigation/RootNavigator.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StatusBar, View } from "react-native";
 import AppNavigator from "./AppNavigator";
 import AuthNavigator from "./AuthNavigator";
 import { usePinCodeStatus } from "app/features/pinCode/ui/PinCodeStatusProvider";
 import DrizzleStudio from "db/DrizzleStudio";
 import colors from "constants/colors";
+import { useActionSheet } from "components/ActionSheet/ActionSheetContext";
 
 const RootNavigator: React.FC = () => {
   const { pinVisible, isLoading } = usePinCodeStatus();
+  const { closeSheet } = useActionSheet();
+
+  useEffect(() => {
+    if (pinVisible) {
+      closeSheet();
+    }
+  }, [pinVisible]);
 
   return (
     <View style={{ flex: 1 }}>
