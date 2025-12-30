d66606389a7cdcda755efecf90c7d8dba1ee442e
sofronijev <sofronijev@gmail.com>
2025-12-30 15:01:39 +0100

    Delete all

--- drizzle/0002_fill_initial_wallets.sql
+++
@@ -1,6 +0,0 @@
--- Custom SQL migration file, put your code below! --
-INSERT
-    OR IGNORE INTO Wallet (walletId, user_id, walletName, color)
-VALUES
-    (1, 1, 'Personal', '#3EB489'),
-    (2, 1, 'Savings', '#1E90FF');
