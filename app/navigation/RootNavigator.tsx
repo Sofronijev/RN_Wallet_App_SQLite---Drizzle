5368fb3487834972e15033c710a761b9bec76dbb
sofronijev <sofronijev@gmail.com>
2026-01-08 17:30:04 +0100

    Fix status bar color

--- app/navigation/RootNavigator.tsx
+++ app/navigation/RootNavigator.tsx
@@ -1,15 +1,20 @@
 import React, { useEffect } from "react";
-import { StatusBar, View } from "react-native";
+import { View } from "react-native";
 import AppNavigator from "./AppNavigator";
 import AuthNavigator from "./AuthNavigator";
 import { usePinCodeStatus } from "app/features/pinCode/ui/PinCodeStatusProvider";
 import DrizzleStudio from "db/DrizzleStudio";
 import colors from "constants/colors";
 import { useActionSheet } from "components/ActionSheet/ActionSheetContext";
+import { StatusBar } from "expo-status-bar";
+import { useAppTheme } from "app/theme/ThemeContext";
 
 const RootNavigator: React.FC = () => {
   const { pinVisible, isLoading } = usePinCodeStatus();
   const { closeSheet } = useActionSheet();
+  const { themeMode } = useAppTheme();
+
+  const statusBarStyle = themeMode === "dark" ? "light" : themeMode === "light" ? "dark" : "auto";
 
   useEffect(() => {
     if (pinVisible) {
@@ -20,7 +25,7 @@ const RootNavigator: React.FC = () => {
   return (
     <View style={{ flex: 1 }}>
       {__DEV__ && <DrizzleStudio />}
-      <StatusBar animated={true} backgroundColor={colors.greenMint} />
+      <StatusBar animated={true} backgroundColor={colors.greenMint} style={statusBarStyle} />
       {pinVisible && !isLoading ? <AuthNavigator /> : <AppNavigator />}
     </View>
   );
