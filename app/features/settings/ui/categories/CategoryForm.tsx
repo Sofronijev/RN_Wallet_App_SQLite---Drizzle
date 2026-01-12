b44b2d40c0d5100ebec047d7f641fe84d682cf4c
sofronijev <sofronijev@gmail.com>
2026-01-12 10:09:33 +0100

    remove shadow

--- app/features/settings/ui/categories/CategoryForm.tsx
+++ app/features/settings/ui/categories/CategoryForm.tsx
@@ -509,11 +509,6 @@ const themedStyles = (theme: AppTheme) =>
       borderRadius: 10,
       alignItems: "center",
       justifyContent: "center",
-      shadowColor: theme.colors.primary,
-      shadowOffset: { width: 0, height: 2 },
-      shadowOpacity: 0.3,
-      shadowRadius: 4,
-      elevation: 3,
     },
     typesList: {
       backgroundColor: theme.colors.card,
