6f08517ee613d7379ab8f57f0e3f91a6d162e77c
sofronijev <sofronijev@gmail.com>
2026-01-16 13:26:32 +0100

    add defautl wallet currency

--- drizzle/0002_initial_wallets.sql
+++ drizzle/0002_initial_wallets.sql
@@ -1,6 +1,12 @@
 -- Custom SQL migration file, put your code below! --
 INSERT
-    OR IGNORE INTO Wallet (walletId, user_id, walletName, color)
-VALUES
-    (1, 1, 'Personal', '#3EB489'),
-    (2, 1, 'Savings', '#1E90FF');
+    OR IGNORE INTO Wallet (
+        walletId,
+        user_id,
+        walletName,
+        color,
+        currencyCode,
+        currencySymbol
+    )
+VALUES (1, 1, 'Personal', '#3EB489', "EUR", "€"),
+    (2, 1, 'Savings', '#1E90FF', "EUR", "€");
